5425bf09c045139ecb0ca331c66b38b08c86b8c1
lucasludu <70537776+lucasludu@users.noreply.github.com>
2021-06-20 22:35:41 -0300

    Add files via upload

---
+++ Clase 3/Clase/Parte 2/js/App.js
@@ -0,0 +1,33 @@
+/*
+function suma(a) {
+    return a;
+}
+
+suma('h');
+// JQuery
+// const cuerpoDOM = $('body');
+// JS (esto es lo mismo que el anterior)
+const cuerpoDOM2 = document.querySelector('body');
+
+//cuerpoDOM2.style.cssText = 'background-color: red;'
+
+// Con Jquery
+// elemento HTML literal del elemento ej: body, p, h1, etc.
+// clase .clase, .estilo
+// identificador #identificador, #contenedor
+$('body').css('background-color', 'blue');
+
+$('#contenedor').css({
+    "background-color" : "red",
+    "border" : "1px solid"
+});
+*/
+/*
+$(document).ready(
+    function() {
+        alert('Hoja JS');
+    }
+);
+*/
+
+alert('Hoja JS');
